567a3c88cb89cffddbbc8eb6257cb2522956448a
Jan Jaworski <jaworek3211@gmail.com>
2021-10-10 22:38:31 +0200

    update

--- src/components/Requirements/Requirements.tsx
+++ src/components/Requirements/Requirements.tsx
@@ -5,9 +5,14 @@ const Requirements = () => {
   return (
     <Card className={styles.root}>
       <div className={styles.title}>Requirements</div>
-      <div>Squadrons: 250-2750 STE (single asset staking only)</div>
-      <div>Tier 1: 250-2999 STE</div>
-      <div>Tier 2, 3, 4: 3000+ STE</div>
+      <div className={styles.grid}>
+        <div>Squadrons:</div>
+        <div> 250-2750 STE (single asset staking only)</div>
+        <div>Tier 1:</div>
+        <div> 250-2999 STE</div>
+        <div>Tier 2, 3, 4:</div>
+        <div> 3000+ STE</div>
+      </div>
     </Card>
   );
 };
